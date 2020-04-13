a9d9473ad4b01eaa4be306d99ca3551fff982540
willian-nascimento <wbsn15@gmail.com>
2020-04-13 14:55:49 -0300

    Código em C: uma função para calcular a área de um circulo.

---
+++ area_do_circulo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+float area (float raio);
+
+int main (void ) {
+
+   float x;
+
+   printf("Digite o raio (m): ");
+   scanf("%f", &x);
+
+   printf("\nArea do circulo: %.2f m².\n", area(x));
+   
+   return 0;
+
+}
+
+float area (float raio) {
+
+   float pi = 3.14159;
+   
+   return pi * (raio * raio);
+
+}
